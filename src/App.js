8082f86b58eed6d44b62d59942d2d602b65b6473
ku13shov <kuleshov000@gmail.com>
2023-07-19 17:16:17 +0300

    added getting data from API

--- src/App.js
+++ src/App.js
@@ -1,17 +1,34 @@
-import React from 'react';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+
 import './index.scss';
 import { Success } from './components/Success';
 import { Users } from './components/Users';
 
-// Тут список пользователей: https://reqres.in/api/users
-
 function App() {
-  return (
-    <div className="App">
-      <Users />
-      {/* <Success /> */}
-    </div>
-  );
+    const [users, setUsers] = useState('');
+    const [isLoading, setIsLoading] = useState(true);
+
+    useEffect(() => {
+        const getUsers = async () => {
+            try {
+                const { data } = await axios.get('https://4805f9918df2294c.mokky.ru/users');
+                setUsers(data);
+                setIsLoading(false);
+            } catch (error) {
+                console.error(error);
+            }
+        };
+
+        getUsers();
+    }, []);
+
+    return (
+        <div className="App">
+            <Users items={users} isLoading={isLoading} />
+            {/* <Success /> */}
+        </div>
+    );
 }
 
 export default App;
